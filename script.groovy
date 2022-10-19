39b7318119d6db3d7e5bdce69c65543809b23d61
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2022-10-19 18:49:36 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,13 @@
-
 def buildImage() {
     echo "building the docker image..."
+    sh "docker build -t saurabhkr952/todo-app:3.0 ."
+    
+} 
+def deployApp() {
+    echo 'deploying the application...'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t saurabhkr952/demo-app:2.0 ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push saurabhkr952/demo-app:2.0'
     }
-} 
-
-def deployApp() {
-    echo 'deploying the application...'
-} 
-
+}
 return this
